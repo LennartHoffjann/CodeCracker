66675c789eebc55cf6c9ae5645b7b7b4c35727b1
LennartHoffjann <lennart.hoffjann@klosebrothers.de>
2022-12-06 15:03:45 +0100

    solve kata

--- src/main/java/org/example/CodeCracker.java
+++ src/main/java/org/example/CodeCracker.java
@@ -5,20 +5,11 @@ import java.util.stream.Collectors;
 public class CodeCracker {
   final String fromKey;
   final String toKey;
-  CodeCracker(final String fromKey, final String toKey){
+
+  CodeCracker(final String fromKey, final String toKey) {
     this.fromKey = fromKey;
     this.toKey = toKey;
   }
-
-  String convert(final String message, final String fromKey, final String toKey) {
-    return message.chars()
-      .mapToObj(i -> (char) i)
-      .map(fromKey::indexOf)
-      .map(i -> i == -1 ? ' ': toKey.charAt(i))
-      .map(Object::toString)
-      .collect(Collectors.joining());
-  }
-
 
   public String encrypt(String message) {
     return convert(message, fromKey, toKey);
@@ -27,7 +18,18 @@ public class CodeCracker {
   public String decrypt(String message) {
     return convert(message, toKey, fromKey);
   }
+
+  String convert(final String message, final String fromKey, final String toKey) {
+    return message.chars()
+      .mapToObj(i -> (char) i)
+      .map(fromKey::indexOf)
+      .map(i -> i == -1 ? ' ' : toKey.charAt(i))
+      .map(Object::toString)
+      .collect(Collectors.joining());
+  }
 }
+
+
 
 
 
